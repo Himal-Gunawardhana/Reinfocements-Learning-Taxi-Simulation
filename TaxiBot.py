ffab2125322336460bad5732030a227715a5f88f
Himal-Gunawardhana <gunawar-ec20005@stu.kln.ac.lk>
2025-01-15 11:19:24 +0530

    set initial state by user itself

--- TaxiBot.py
+++ TaxiBot.py
@@ -1,14 +1,15 @@
 import random
-import gymnasium as gym
+import gym
 import numpy as np
 
-env = gym.make('Taxi-v3',render_mode='ansi')
+env = gym.make('Taxi-v3')
 
-alpha = 0.9
-gamma = 0.95
-epsilon = 1.0
-epsilon_decay = 0.9995
-min_epsilon = 0.01
+alpha = 0.9     # how percentage is important new rewards(learning rate)
+gamma = 0.95    # how important future rewards are (discount factor)
+epsilon = 1.0   # all the actions in the beginning will be random and 
+# if epsilon is zero that means always depending on q-values intead of random(exploration rate)
+epsilon_decay = 0.9995  # how fast epsilon will decay
+min_epsilon = 0.01  # minimum value of epsilon (at least 1% of the randomness)
 num_episodes = 10000
 max_steps = 100
 
@@ -16,27 +17,28 @@ q_table = np.zeros((env.observation_space.n, env.action_space.n))
 
 def choose_action(state):
     if random.uniform(0, 1) < epsilon:
-        return env.action_space.sample()
+        return env.action_space.sample()    # take random action
     else:
-        return np.argmax(q_table[state, :])
+        return np.argmax(q_table[state, :])     # take action with highest q-value
 
-for episode in range(num_episodes):
-    state, _ = env.reset()
+for episode in range(num_episodes):     #training loop
+    state, _ = env.reset()      # reset the environment
+
     done = False
 
-    for step in range(max_steps):
-        action = choose_action(state)
+    for step in range(max_steps):    # maximum 100 steps taken to reach the goal randomly while training
+        action = choose_action(state)       # choose an action
 
-        next_state, reward, done, truncated, info = env.step(action)
+        next_state, reward, done, truncated, info = env.step(action)    # take the action
 
-        old_value = q_table[state, action]
-        next_max = np.max(q_table[next_state, :])
+        old_value = q_table[state, action]   # old q-value
+        next_max = np.max(q_table[next_state, :])   # next state max q-value
 
-        q_table[state, action] = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+        q_table[state, action] = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)      # update q-value
 
-        state = next_state
+        state = next_state   # update state
 
-        if done or truncated:
+        if done or truncated:   # if done or truncated break the loop
             break
 
     epsilon = max(min_epsilon, epsilon * epsilon_decay)
--- Taxi_Q.py
+++ Taxi_Q.py
@@ -3,58 +3,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-def run(episodes, is_training=True, render=False):
+class CustomTaxiEnv(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.initial_taxi_row = None
+        self.initial_taxi_col = None
+        self.initial_passenger_loc = None
+        self.initial_destination = None
 
-    env = gym.make('Taxi-v3', render_mode='human' if render else None)
+    def reset(self, seed=None, options=None):
+        state, info = self.env.reset(seed=seed)
+        
+        # Force set the initial state
+        if all(x is not None for x in [self.initial_taxi_row, self.initial_taxi_col, 
+                                     self.initial_passenger_loc, self.initial_destination]):
+            
+            # Directly modify the underlying environment state
+            self.unwrapped.s = self.unwrapped.encode(
+                self.initial_taxi_row,
+                self.initial_taxi_col,
+                self.initial_passenger_loc,
+                self.initial_destination
+            )
+            
+            # Update the state to reflect our changes
+            state = self.unwrapped.s
+            
+        return state, info
 
-    if(is_training):
-        q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 500 x 6 array
+def run(episodes, is_training=True, render=False, initial_config=None):
+    env = CustomTaxiEnv(gym.make('Taxi-v3', render_mode='human' if render else None))
+    
+    # Set initial configuration if provided
+    if initial_config:
+        env.initial_taxi_row = initial_config['taxi_row']
+        env.initial_taxi_col = initial_config['taxi_col']
+        env.initial_passenger_loc = initial_config['passenger_loc']
+        env.initial_destination = initial_config['destination']
+
+    if is_training:
+        q = np.zeros((env.observation_space.n, env.action_space.n))
     else:
-        f = open('taxi.pkl', 'rb')
-        q = pickle.load(f)
-        f.close()
+        with open('taxi.pkl', 'rb') as f:
+            q = pickle.load(f)
 
-    learning_rate_a = 0.9 # alpha or learning rate
-    discount_factor_g = 0.9 # gamma or discount rate. Near 0: more weight/reward placed on immediate state. Near 1: more on future state.
-    epsilon = 1         # 1 = 100% random actions
-    epsilon_decay_rate = 0.0001        # epsilon decay rate. 1/0.0001 = 10,000
-    rng = np.random.default_rng()   # random number generator
-
+    learning_rate_a = 0.9
+    discount_factor_g = 0.9
+    epsilon = 1
+    epsilon_decay_rate = 0.0001
+    rng = np.random.default_rng()
     rewards_per_episode = np.zeros(episodes)
 
     for i in range(episodes):
-        state = env.reset()[0]  # states: 0 to 63, 0=top left corner,63=bottom right corner
-        terminated = False      # True when fall in hole or reached goal
-        truncated = False       # True when actions > 200
-
+        state, _ = env.reset()
+        terminated = False
+        truncated = False
         rewards = 0
-        while(not terminated and not truncated):
+
+        while not terminated and not truncated:
             if is_training and rng.random() < epsilon:
-                action = env.action_space.sample() # actions: 0=left,1=down,2=right,3=up
+                action = env.action_space.sample()
             else:
-                action = np.argmax(q[state,:])
+                action = np.argmax(q[state, :])
 
-            new_state,reward,terminated,truncated,_ = env.step(action)
-
+            new_state, reward, terminated, truncated, _ = env.step(action)
             rewards += reward
 
             if is_training:
-                q[state,action] = q[state,action] + learning_rate_a * (
-                    reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action]
+                q[state, action] = q[state, action] + learning_rate_a * (
+                    reward + discount_factor_g * np.max(q[new_state, :]) - q[state, action]
                 )
 
             state = new_state
 
         epsilon = max(epsilon - epsilon_decay_rate, 0)
-
-        if(epsilon==0):
+        if epsilon == 0:
             learning_rate_a = 0.0001
-
 
         rewards_per_episode[i] = rewards
 
     env.close()
 
+    # Plot rewards
     sum_rewards = np.zeros(episodes)
     for t in range(episodes):
         sum_rewards[t] = np.sum(rewards_per_episode[max(0, t-100):(t+1)])
@@ -62,11 +92,23 @@ def run(episodes, is_training=True, render=False):
     plt.savefig('taxi.png')
 
     if is_training:
-        f = open("taxi.pkl","wb")
-        pickle.dump(q, f)
-        f.close()
+        with open("taxi.pkl", "wb") as f:
+            pickle.dump(q, f)
 
 if __name__ == '__main__':
-    run(15000)
+    # Define initial configuration
+    # Locations: 0=Red, 1=Green, 2=Yellow, 3=Blue
+    initial_config = {
+        'taxi_row': 0,        # 0-4 (top to bottom)
+        'taxi_col': 0,        # 0-4 (left to right)
+        'passenger_loc': 3,   # 0=Red location
+        'destination': 2      # 2=Yellow location
+    }
 
-    run(10, is_training=False, render=True)
+    # Training phase
+    print("Starting training...")
+    run(15000, is_training=True, initial_config=initial_config)
+
+    # Evaluation phase with rendering
+    print("Starting evaluation...")
+    run(1, is_training=False, render=True, initial_config=initial_config)
